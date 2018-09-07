18e8b45cae24f927e617f8b9b4254089806f9083
Glitch (bbot-richmessagebot-utkarsh) <none>
2018-09-07 14:55:14 +0000

    🎆😺 Checkpoint ./index.js:1424654/38

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ bot.global.text({
 }, { id: 'door-prize-intro' })
 
 bot.global.text(/text button with url/i, (b) => {
-  return b.respond({
+  return b.bot.adapters.message.driver({
         // fallback: 'buttnnnn',
         // title: 'text button with url',
       actions: [

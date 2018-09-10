eb0b6e40d54027ecae004537f34c8f318f29ebcd
Glitch (bbot-richmessagebot-utkarsh) <none>
2018-09-10 10:50:24 +0000

    🚄🙋 Checkpoint ./richmessage.js:1424654/2386

--- richmessage.js
+++ richmessage.js
@@ -4,61 +4,55 @@ bot.global.text(/text button with url/i, (b) => {
   b.envelope.write('text button with url')
   b.envelope.payload.quickReply({
       text: 'Book flights',
-      url: 'http://www.kayak.com'
+      url: 'http://www.kayak.com',
+      is_webview: false
   })
   return b.respond().catch((err) => console.error(err))
 })
 
 bot.global.text(/text button with msg in chat window/i, (b) => {
   b.envelope.write('text button with msg in chat window')
-  b.envelope.payload
-    .quickReply({ text: 'hello in chat window' })
+  b.envelope.payload.quickReply({ 
+    text: 'hello in chat window'
+  })
   return b.respond().catch((err) => console.error(err))
 })
 
-bot.global.text(/abcd/i, (b) => {
-  b.envelope.write('Choose your fate! 🚪... 🎁 ')
-  b.envelope.attach({ color: '#f4426e' })
-  b.envelope.payload
-    .quickReply({ text: 'Door number 1' })
-    .quickReply({ text: 'Door number 2' })
-    .quickReply({ text: 'Door number 3' })
+bot.global.text(/image button with url/i, (b) => {
+  b.envelope.write('image button with url')
+  b.envelope.payload.quickReply({
+      image_url: 'http://www.emoji.co.uk/files/phantom-open-emojis/travel-places-phantom/12698-airplane.png',
+      url: 'http://www.kayak.com'
+  })
   return b.respond().catch((err) => console.error(err))
-}, { id: 'door-prize-intro' })
+})
 
-bot.global.text(/json/i, (b) => {
+bot.global.text(/image button with msg in chat window/i, (b) => {
+  b.envelope.write('image button with msg in chat window')
+  b.envelope.payload.quickReply({ 
+    image_url: 'http://www.emoji.co.uk/files/phantom-open-emojis/travel-places-phantom/12698-airplane.png',
+    msg: 'I clicked the airplane'
+  })
+  return b.respond().catch((err) => console.error(err))
+})
+
+bot.global.text(/horizontal text buttons/i, (b) => {
   b.envelope.write('Horizontal buttons:')
-  b.envelope.attach({ title: 'horizontal text buttons with url', button_alignment: 'horizontal' })
-  b.envelope.payload.quickReply({
-    "type": "button",
-    "text": "Book flights",
-    "url": "http://www.kayak.com",
-    "is_webview": false
+  b.envelope.attach({ 
+    title: 'horizontal text buttons with url',
+    button_alignment: 'horizontal'
   })
   b.envelope.payload.quickReply({
-    "type": "button",
-    "text": "Cancel travel request",
-    "url": "https://requests.example.com/cancel/r123456",
-    "is_webview": false
+    type: 'button',
+    text: 'Book flights',
+    url: 'http://www.kayak.com',
+    is_webview: false
   })
-  return b.respond(
-    // {
-    //   "title": "horizontal text buttons with url",
-    //   "button_alignment": "horizontal",
-    //   "actions": [
-    //     {
-    //       "type": "button",
-    //       "text": "Book flights",
-    //       "url": "http://www.kayak.com",
-    //       "is_webview": false
-    //     },
-    //     {
-    //       "type": "button",
-    //       "text": "Cancel travel request",
-    //       "url": "https://requests.example.com/cancel/r123456",
-    //       "is_webview": false
-    //     }
-    //   ]
-  // }
-).catch((err) => console.error(err))
+  b.envelope.payload.quickReply({
+    type: 'button',
+    text: 'Cancel travel request',
+    url: 'https://requests.example.com/cancel/r123456',
+    is_webview: false
+  })
+  return b.respond().catch((err) => console.error(err))
 })  

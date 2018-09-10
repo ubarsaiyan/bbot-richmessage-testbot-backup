7e20ba602da0d2d0ef5890f335b457f14444da38
Glitch (bbot-richmessagebot-utkarsh) <none>
2018-09-10 11:11:00 +0000

    👩‍👩‍👦🐜 Checkpoint ./richmessage.js:1424654/3373

--- richmessage.js
+++ richmessage.js
@@ -1,5 +1,6 @@
 const bot = require('bbot')
 
+// 1. Text Button With Url
 bot.global.text(/text button with url/i, (b) => {
   b.envelope.write('text button with url')
   b.envelope.payload.quickReply({
@@ -10,6 +11,7 @@ bot.global.text(/text button with url/i, (b) => {
   return b.respond().catch((err) => console.error(err))
 })
 
+// 2. Text Button With Msg in Chat Window (Not fully working: Not passing the msg field)
 bot.global.text(/text button with msg in chat window/i, (b) => {
   b.envelope.write('text button with msg in chat window')
   b.envelope.payload.quickReply({ 
@@ -19,6 +21,7 @@ bot.global.text(/text button with msg in chat window/i, (b) => {
   return b.respond().catch((err) => console.error(err))
 })
 
+// 3. Image Button With Url
 bot.global.text(/image button with url/i, (b) => {
   b.envelope.write('image button with url')
   b.envelope.payload.quickReply({
@@ -28,6 +31,7 @@ bot.global.text(/image button with url/i, (b) => {
   return b.respond().catch((err) => console.error(err))
 })
 
+// 4. Image Button With Msg in Chat Window
 bot.global.text(/image button with msg in chat window/i, (b) => {
   b.envelope.write('image button with msg in chat window')
   b.envelope.payload.quickReply({ 
@@ -37,11 +41,93 @@ bot.global.text(/image button with msg in chat window/i, (b) => {
   return b.respond().catch((err) => console.error(err))
 })
 
+// 5. Multiple Text Buttons
+bot.global.text(/multiple text buttons/i, (b) => {
+  b.envelope.write('Multiple buttons:')
+  b.envelope.attach({ 
+    title: 'Multiple text buttons with url'
+  })
+  b.envelope.payload.quickReply({
+    type: 'button',
+    text: 'Book flights',
+    url: 'http://www.kayak.com',
+    is_webview: false
+  })
+  b.envelope.payload.quickReply({
+    type: 'button',
+    text: 'Cancel travel request',
+    url: 'https://requests.example.com/cancel/r123456',
+    is_webview: false
+  })
+  return b.respond().catch((err) => console.error(err))
+})  
+
+// 6. Horizontal Text Buttons
 bot.global.text(/horizontal text buttons/i, (b) => {
   b.envelope.write('Horizontal buttons:')
   b.envelope.attach({ 
     title: 'horizontal text buttons with url',
     button_alignment: 'horizontal'
+  })
+  b.envelope.payload.quickReply({
+    type: 'button',
+    text: 'Book flights',
+    url: 'http://www.kayak.com',
+    is_webview: false
+  })
+  b.envelope.payload.quickReply({
+    type: 'button',
+    text: 'Cancel travel request',
+    url: 'https://requests.example.com/cancel/r123456',
+    is_webview: false
+  })
+  return b.respond().catch((err) => console.error(err))
+})  
+
+// 9. Url Button With Full Webview (default)
+bot.global.text(/button with full webview/i, (b) => {
+  b.envelope.write('button with full webview')
+  b.envelope.payload.quickReply({
+      text: 'Book flights',
+      url: 'http://www.kayak.com',
+      is_webview: true
+  })
+  return b.respond().catch((err) => console.error(err))
+})
+
+//10. Url Button With Tall Webview (Not working: Not passing the webview_height_ratio)
+bot.global.text(/button with tall webview/i, (b) => {
+  b.envelope.write('button with tall webview')
+  b.envelope.payload.quickReply({
+      text: 'Book flights',
+      url: 'http://www.kayak.com',
+      is_webview: true,
+      webview_height_ratio: 'tall'
+  })
+  return b.respond().catch((err) => console.error(err))
+})
+
+//11. Url Button With Compact Webview (Not working: Not passing the webview_height_ratio)
+bot.global.text(/button with compact webview/i, (b) => {
+  b.envelope.write('button with compact webview')
+  b.envelope.payload.quickReply({
+      text: 'Book flights',
+      url: 'http://www.kayak.com',
+      is_webview: true,
+      webview_height_ratio: 'compact'
+  })
+  return b.respond().catch((err) => console.error(err))
+})
+
+// 12. Attachment with title, image, link and buttons
+bot.global.text(/attachment with buttons/i, (b) => {
+  b.envelope.write('Attachment with buttons:')
+  b.envelope.attach({ 
+    "title": "Lauri M(title field)",
+      "title_link": "https://www.basketball-reference.com/players/m/markkla01.html",
+      "text": "Should have been rookie of the year (text field)",
+      "description": "What a great player! (description field)",
+      "image_url": "http://www.trbimg.com/img-5b04c449/turbine/ct-spt-bulls-lauri-markkanen-all-rookie-team-20180522",
   })
   b.envelope.payload.quickReply({
     type: 'button',
